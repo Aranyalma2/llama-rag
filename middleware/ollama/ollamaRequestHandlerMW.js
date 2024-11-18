01d28c0b5acdb50fe38fb14971b722cd26c2c278
Sándor Balázs <baluson98@hotmail.com>
2024-11-18 17:18:20 +0100

    using system prompt

--- middleware/ollama/ollamaRequestHandlerMW.js
+++ middleware/ollama/ollamaRequestHandlerMW.js
@@ -16,21 +16,22 @@ module.exports = function () {
 
             console.log("prompt:", prompt);
 
-            let systemPrompt = prompt.match(/<\|im_start\|>system([\s\S]*?)<\|im_end\|>/);
+            let systemPrompt = prompt.match(/(?<=<\|im_start\|>system)([\s\S]*?)(?=<\|im_end\|>)/);
             systemPrompt = systemPrompt ? systemPrompt[0] : null;
-            let userPrompt = prompt.match(/<\|im_start\|>user([\s\S]*?)<\|im_end\|>/);
+            let userPrompt = prompt.match(/(?<=<\|im_start\|>user)([\s\S]*?)(?=<\|im_end\|>)/);
             userPrompt = userPrompt ? userPrompt[0] : null;
 
             //ASK TASK GENERATOR MODEL
             const taskQuestionPrompt = `
-                Convert the following question into a short strict standalone task. Use english language only. Do not explan the question, just translate and summurize it.
+                Convert the following question into a short strict standalone instruction. Use english language only. Do not explain the question, just translate and summurize it. You can turn off only the devices that are on.
                 Question: ${userPrompt}
+                The current state of devices: ${systemPrompt}
             `;
-            
+
             console.log('taskQuestionPrompt:', taskQuestionPrompt);
 
             const memeoryService1 = getGlobalMemoryServiceContainer().getInstance('model1');
-            
+
             const taskGeneratorAnswer = await memeoryService1.getRelevantMemory(taskQuestionPrompt);
             console.log('Chain1:', taskGeneratorAnswer);
 
@@ -38,10 +39,10 @@ module.exports = function () {
             let syntaxGeneratorAnswer = null;
             let numberOfTries = 0;
 
-            do{
+            do {
                 syntaxGeneratorAnswer = await memeoryService2.getRelevantMemory(taskGeneratorAnswer);
-                console.log(`Chain2 - ${numberOfTries+1}. try:`, syntaxGeneratorAnswer);
-            }while(!responseValidator(syntaxGeneratorAnswer) && numberOfTries++ < 10);
+                console.log(`Chain2 - ${numberOfTries + 1}. try:`, syntaxGeneratorAnswer);
+            } while (!responseValidator(syntaxGeneratorAnswer) && numberOfTries++ < 10);
 
             res.ollamaResponse = syntaxGeneratorAnswer;
             return next();
