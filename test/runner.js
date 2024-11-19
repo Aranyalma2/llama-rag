3b30c1ba7b8b81e1ab26f9a2f5cca34836ada9f4
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2024-11-19 14:18:23 +0100

    new test runner

--- test/runner.js
+++ test/runner.js
@@ -1,26 +1,9 @@
-const { sendGenerateRequest, validateResponse, promptCreator } = require('./test');
+const { runTest } = require('./test');
 const { userPrompt, systemPrompt, response } = require('./data/test1');
 
-const prompt = promptCreator(systemPrompt, userPrompt);
-
-const NUMBER_OF_RUNS = 10;
 
 (async () => {
 
-    console.log('Running test...');
-
-    let numberOfSuccesses = 0;
-
-    for (let i = 0; i < NUMBER_OF_RUNS; i++) {
-        try {
-            const result = await sendGenerateRequest('model1', prompt);
-            if (validateResponse(result, response)) {
-                numberOfSuccesses++;
-            }
-        } catch (error) {
-            console.error(`Run ${i + 1} failed with error: ${error.message}`);
-        }
-    }
-
-    console.log(`Success rate: ${numberOfSuccesses}/${NUMBER_OF_RUNS}`);
+    await runTest('Test1', systemPrompt, userPrompt, response);
+    
 })();
--- test/test.js
+++ test/test.js
@@ -28,4 +28,28 @@ function promptCreator(systemPrompt, userPrompt) {
     return `<|im_start|>system${systemPrompt}<|im_end|><|im_start|>user${userPrompt}<|im_end|>`;
 }
 
-module.exports = { sendGenerateRequest, validateResponse, promptCreator };
+async function runTest(testName, systemPrompt, userPrompt, expectedResponse) {
+
+    const NUMBER_OF_RUNS = 10;
+
+    console.log(`Running test: ${testName}`);
+
+    const prompt = promptCreator(systemPrompt, userPrompt);
+
+    let numberOfSuccesses = 0;
+
+    for (let i = 0; i < NUMBER_OF_RUNS; i++) {
+        try {
+            const result = await sendGenerateRequest('model', prompt);
+            if (validateResponse(result, expectedResponse)) {
+                numberOfSuccesses++;
+            }
+        } catch (error) {
+            console.error(`Run ${i + 1} failed with error: ${error.message}`);
+        }
+    }
+
+    console.log(`Test ${testName} finished with success rate: ${numberOfSuccesses}/${NUMBER_OF_RUNS}.`);
+}
+
+module.exports = { runTest };
