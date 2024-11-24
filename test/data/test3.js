597be93b8451ced8b4c847930037781af409309b
Sándor Balázs <baluson98@hotmail.com>
2024-11-24 13:12:46 +0100

    updated rag docs

--- test/data/test3.js
+++ test/data/test3.js
@@ -2,7 +2,7 @@ const userPrompt = "I finished my book";
 
 const systemPrompt = `<|im_start|>system
 light.reading_lamp 'attic reading_lamp' = off
-light.reading_lamp 'reading lamp' = off
+light.reading_lamp 'reading lamp' = on
 sensor.solax_srqn3kjshz_yield_today 'solarpanel Yield today' = 3.9 kWh
 sensor.solax_srqn3kjshz_yield_today 'solarpanel production' = 3.9 kWh 
 light.ceiling_lamp 'attic ceiling_lamp' = off
--- test/runner.js
+++ test/runner.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 
 const verboseError = args.includes('--verbose');
 
-if(verboseError) {
+if (verboseError) {
     console.log('Verbose error mode is on.');
 }
 
@@ -15,14 +15,28 @@ if(verboseError) {
      */
     {
         const { userPrompt, systemPrompt, response } = require('./data/test1');
-        await runTest('Test1', systemPrompt, userPrompt, response, verboseError);
+        await runTest('Test1', systemPrompt, userPrompt, response, true);
     }
     /**
      * Test2 import and run
      */
+
     {
         const { userPrompt, systemPrompt, response } = require('./data/test2');
         await runTest('Test2', systemPrompt, userPrompt, response, verboseError);
     }
-    
+    /**
+     * Test3 import and run
+     */
+    {
+        const { userPrompt, systemPrompt, response } = require('./data/test3');
+        await runTest('Test3', systemPrompt, userPrompt, response, verboseError);
+    }
+    /**
+     * Test4 import and run
+     */
+    {
+        const { userPrompt, systemPrompt, response } = require('./data/test4');
+        await runTest('Test4', systemPrompt, userPrompt, response, verboseError);
+    }
 })();
